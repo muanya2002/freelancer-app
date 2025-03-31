94457bd51ff977b43848e48ed0f4d2f9e9a91ba9
muanya2002 <okoyechristine2002@gmail.com>
2025-03-31 12:11:22 +0100

    updates

--- frontend/home.tsx
+++ frontend/home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card"
 import { ArrowRight, Briefcase, CreditCard, Users } from "lucide-react"
 
 export default function Home() {
